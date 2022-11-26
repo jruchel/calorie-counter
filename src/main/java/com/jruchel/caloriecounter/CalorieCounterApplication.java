b402454449282684dea14e5634476990ec91d1fa
jruchel <jruchel254@gmail.com>
2022-11-26 20:59:45 +0100

    initial commit

---
+++ src/main/java/com/jruchel/caloriecounter/CalorieCounterApplication.java
@@ -0,0 +1,12 @@
+package com.jruchel.caloriecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieCounterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jruchel/caloriecounter/CalorieCounterApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jruchel.caloriecounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCounterApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
